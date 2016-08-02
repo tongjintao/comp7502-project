151d4f763eaf265360df6daef3fb5199d91f5a08
Virgil <Virgil@github.com>
2016-08-02 23:25:14 +0800

    working histogram-contour program except 10

--- histogram_contour_classifier.py
+++ histogram_contour_classifier.py
@@ -1,11 +1,15 @@
 import numpy as np
 import cv2
+import os
 
-templateObjs = {}
-#rank_list = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "jack", "queen", "king"]
-rank_list = [ "queen"]
+rank_templates = {}
+suit_templates = {}
+rank_list = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "jack", "queen", "king"]
+#rank_list = ["eight"]
+suit_list = ["heart", "spade", "club", "diamond"]
+# suit_list = ["heart"]
 
-def highlight_contour(img, ctr, name='highlight'):
+def highlight_contour(img, ctr, name="highlight"):
 	# highlight proceed contour in green
 	img_highlight = img.copy()
 	for point in ctr:
@@ -27,7 +31,6 @@ def crop_contour(img_threshold, ctr):
 def crop_img(img_threshold, x, y, w, h):
 	# crop out image
 	ctr_crop = img_threshold[y:y+h, x:x+w]
-	show_image('img_threshold', ctr_crop)
 	return ctr_crop
 
 def transform_list(numbers, desired_range):
@@ -85,19 +88,16 @@ def find_threshold(img_histogram):
 					local_maxima_n.append(local_maxima[i])
 
 		local_maxima = local_maxima_n
-
-	#print local_maxima
-
 	# sort it 
 	local_maxima.sort(key = lambda d: d[0], reverse=True)
-	#print local_maxima[0], local_maxima[1]
 
 	return (local_maxima[0][1] + local_maxima[1][1])/ 2
-	
-def load_template():
 
-	for templateName in rank_list:
-		template = cv2.imread("rank_template/" + templateName + ".png")
+def load_template_suit():
+
+	for suit in suit_list:
+
+		template = cv2.imread("suit_template/" + suit + ".png")
 		
 		# convert to grayscale and apply Gaussian filtering and thresholding
 		template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -123,13 +123,42 @@ def load_template():
 			# return the last contour as template
 			if hierarchy[0][index][0] == -1:
 				contour_whole = contours[index]
-			#crop_contour(template_gray, contour)
-		
-		#highlight_contour(template_gray, contour_whole)
 
 		x_histogram, y_histogram = make_histogram(contour_whole)
-		# highlight_contour(template_gray, contour_whole)
-		templateObjs[templateName] = (contour_whole, x_histogram, y_histogram)
+		suit_templates[suit] = (contour_whole, x_histogram, y_histogram)
+	
+def load_template_rank():
+
+	for rank in rank_list:
+		template = cv2.imread("rank_template/" + rank + ".png")
+		
+		# convert to grayscale and apply Gaussian filtering and thresholding
+		template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+		template_gray = cv2.GaussianBlur(template_gray, (3,3),0)	
+		_, template_gray = cv2.threshold(template_gray, 170, 255, cv2.THRESH_BINARY)
+
+		contours, hierarchy = cv2.findContours(template_gray.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+		for index in range(len(contours)):
+			# skip outer contour
+			if hierarchy[0][index][3] == -1:
+				continue
+			# if have children, include all sub-contour into contour_whole and break
+			if hierarchy[0][index][2] != -1:
+				child = [index]
+				contour_whole = contours[index]
+				for child_i in range(index+1, len(contours)):
+					if hierarchy[0][child_i][3] == index:
+						contour_whole = np.concatenate((contour_whole, contours[child_i]))
+					else:
+						break
+				break
+			# return the last contour as template
+			if hierarchy[0][index][0] == -1:
+				contour_whole = contours[index]
+
+		x_histogram, y_histogram = make_histogram(contour_whole)
+		rank_templates[rank] = (contour_whole, x_histogram, y_histogram)
 
 def compare_histogram_chisquare(hist1, hist2):
 	# calculate difference in histogram by chisquare
@@ -143,6 +172,79 @@ def compare_histogram_chisquare(hist1, hist2):
 			diff_chisquare += (hist1[i] - hist2[i]) * (hist1[i] - hist2[i]) / hist1[i]
 
 	return diff_chisquare
+
+def recognize_rank(contours, hier):
+	possible_ranks = {}
+	for rank, rank_value in rank_templates.items():
+		rank_contour, rank_x_histogram, rank_y_histogram = rank_value
+		for index in range(len(contours)):
+			# screen out contour that are too small
+			if len(contours[index]) < 30:
+				continue
+			
+			# if have children, include all sub-contour into contour_whole and break
+			if hier[0][index][2] != -1:
+				child = [index]
+				contour_whole = contours[index]
+				for child_i in range(index+1, len(contours)):
+					if hier[0][child_i][3] == index:
+						contour_whole = np.concatenate((contour_whole, contours[child_i]))
+					else:
+						break
+			else:
+				contour_whole = contours[index]
+
+			# make histogram and compare difference with chisquare method
+			x_histogram, y_histogram = make_histogram(contour_whole)
+			x_diff_chisquare = compare_histogram_chisquare(x_histogram[0].tolist(), rank_x_histogram[0].tolist())
+			y_diff_chisquare = compare_histogram_chisquare(y_histogram[0].tolist(), rank_y_histogram[0].tolist())
+	
+			# highlight proceed contour in green if value is lower enough
+			x_y_diff = x_diff_chisquare * y_diff_chisquare 
+			if x_y_diff < 0.05:
+				if possible_ranks.has_key(rank) and possible_ranks.get(rank) > x_y_diff:
+					possible_ranks[rank] = x_y_diff
+				if not possible_ranks.has_key(rank):
+					possible_ranks[rank] = x_y_diff
+	return possible_ranks
+
+def recognize_suit(contours, hier):
+	possible_suits = {}
+	for suit, suit_value in suit_templates.items():
+
+		suit_contour, suit_x_histogram, suit_y_histogram = suit_value
+		for index in range(len(contours)):
+			# screen out contour that are too small
+			if len(contours[index]) < 30:
+				continue
+			
+			# if have children, include all sub-contour into contour_whole and break
+			if hier[0][index][2] != -1:
+				child = [index]
+				contour_whole = contours[index]
+				for child_i in range(index+1, len(contours)):
+					if hier[0][child_i][3] == index:
+						contour_whole = np.concatenate((contour_whole, contours[child_i]))
+					else:
+						break
+			else:
+				contour_whole = contours[index]
+			
+
+
+			# make histogram and compare difference with chisquare method
+			x_histogram, y_histogram = make_histogram(contour_whole)
+			x_diff_chisquare = compare_histogram_chisquare(x_histogram[0].tolist(), suit_x_histogram[0].tolist())
+			y_diff_chisquare = compare_histogram_chisquare(y_histogram[0].tolist(), suit_y_histogram[0].tolist())
+			x_y_diff = x_diff_chisquare * y_diff_chisquare 
+
+			if x_y_diff < 0.05:
+				if possible_suits.has_key(suit) and possible_suits.get(suit) > x_y_diff:
+					possible_suits[suit] = x_y_diff
+				if not possible_suits.has_key(suit):
+					possible_suits[suit] = x_y_diff
+
+	return possible_suits
 
 def recognize_card(img):
 	# convert to grayscale and apply Gaussian filtering
@@ -158,120 +260,36 @@ def recognize_card(img):
 	
 	# threshold the image
 	ret, img_threshold = cv2.threshold(img_gray, threshold, 255, cv2.THRESH_BINARY)
-	show_image('image', img_threshold)
+	#show_image('image', img_threshold)
 
 	height, width = img_threshold.shape
 
 	# crop out the top left corner
-	img_threshold = crop_img(img_threshold, 0, 0, width/4, height/4)
+	img_threshold = crop_img(img_threshold, 0, 0, width/3, height/3)
 
 	# find contours in the image
-	ctrs, hier = cv2.findContours(img_threshold.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)	
-	possible_ranks = {}
+	ctrs, hier = cv2.findContours(img_threshold.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)	
+	
+	possible_suits = recognize_suit(ctrs, hier)
+	suit = min(possible_suits.items(), key=lambda x: x[1])[0]
 
-	for rank, rank_value in templateObjs.items():
-		rank_contour, rank_x_histogram, rank_y_histogram = rank_value
-		for index in range(len(ctrs)):
-			# screen out contour that are too small
-			if len(ctrs[index]) < 30:
-				continue
-			
-			highlight_contour(img, ctrs[index], "contour_each")
-			print hier[0][index]
-			# if have children, include all sub-contour into contour_whole and break
-			if hier[0][index][2] != -1:
-				child = [index]
-				contours_whole = ctrs[index]
-				for child_i in range(index+1, len(ctrs)):
-					if hier[0][child_i][3] == index:
-						highlight_contour(img, ctrs[index], "contour_child")
-						contour_whole = np.concatenate((contour_whole, ctrs[child_i]))
-					else:
-						break
-				break
-			else:
-				contour_whole = ctrs[index]
+	possible_ranks = recognize_rank(ctrs, hier)
+	rank = min(possible_ranks.items(), key=lambda x: x[1])[0]
+	
+	return suit, rank
 
-			highlight_contour(img, contour_whole, "contour_whole")
-			x_histogram, y_histogram = make_histogram(contour_whole)
-
-			## Print out value 
-			# print "rank " + rank
-			# print rank_x_histogram
-			# print rank_y_histogram
-
-			# print "contours to be compared"
-			# print x_histogram
-			# print y_histogram
-
-			x_diff_chisquare = compare_histogram_chisquare(x_histogram[0].tolist(), rank_x_histogram[0].tolist())
-			# print("chisquare x difference: {0}").format(x_diff_chisquare)
-			y_diff_chisquare = compare_histogram_chisquare(y_histogram[0].tolist(), rank_y_histogram[0].tolist())
-			# print("chisquare y difference: {0}").format(y_diff_chisquare)	
-
-			# print("chisquare x * y difference: {0}".format(x_diff_chisquare * y_diff_chisquare))
-
-			# # try print matchSahpes difference
-			# retval = cv2.matchShapes(rank_contour, ctrs[index], 2, 0)
-			# print retval
-			
-			# highlight proceed contour in green if value is lower enough
-			x_y_diff = x_diff_chisquare * y_diff_chisquare 
-			if x_y_diff < 0.05:
-				if possible_ranks.has_key(rank) and possible_ranks.get(rank) > x_y_diff:
-					possible_ranks[rank] = x_y_diff
-				if not possible_ranks.has_key(rank):
-					possible_ranks[rank] = x_y_diff
-
-				# highlight_contour(img, ctrs[index])
-				# print rank
-				# print("chisquare x difference: {0}").format(x_diff_chisquare)
-			 # 	print("chisquare y difference: {0}").format(y_diff_chisquare)	
-			 # 	print("chisquare x * y difference: {0}".format(x_diff_chisquare * y_diff_chisquare))
-
-	# for rank, x_y_diff in possible_ranks.items():
-	# 	print rank, x_y_diff
-
-	print min(possible_ranks.items(), key=lambda x: x[1])[0]
-
-	# Get rectangles contains each contour 
-	rects = [cv2.boundingRect(ctr) for ctr in ctrs] 
-
-	for rect in rects:
-		# draw the rectangles
-		cv2.rectangle(img, (rect[0],rect[1]), (rect[0]+rect[2],rect[1]+rect[3]), (127,255,0),1)
-
-	# show image
-	show_image('image', img)
+load_template_rank()
+load_template_suit()
 
 
-load_template()
+imgs = os.listdir(os.getcwd()+'/playingcard')
 
-#img = cv2.imread('rank_template/three.png')
-#img = cv2.imread('playingcard/diamond3.jpg')
-#img = cv2.imread('sift/club3.jpg')
-#img = cv2.imread('test_images/heart3.jpg')
+for img in imgs:
 
-#img = cv2.imread('sift/club7.jpg')
-#img = cv2.imread('test_images/club7.jpg')
-
-img = cv2.imread('playingcard/heartq.jpg')
-
-recognize_card(img)
-
-
-
-
-"""
-For reference:
-
---------------------
-
-
-for ctr in ctrs:
-	print "contour"
-	for point in ctr:
-		contour_point = map(tuple,point)[0]
-		cv2.circle(img, contour_point , 1, (127,255,0), 1)
-
-"""
+	if 'cluba.jpg' not in img:
+		continue
+	print img
+	img = cv2.imread('playingcard/'+img)
+	# recognize_card(img)
+	suit, rank = recognize_card(img)
+	print suit, rank
