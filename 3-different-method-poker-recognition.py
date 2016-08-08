79709165055b16dcdd9310662dadc5dd742fed7e
Virgil <Virgil@github.com>
2016-08-08 23:58:10 +0800

    all 3 included

--- 3-different-method-poker-recognition.py
+++ 3-different-method-poker-recognition.py
@@ -4,6 +4,7 @@ import os
 import histogram_contour_classifier as hcc
 import trapezoid_utils as tu
 import wavelet_utils as wave
+import siftcard
 
 hcc.load_template_rank()
 hcc.load_template_suit()
@@ -21,13 +22,17 @@ while(True):
     elif k == ord(' '):  
     	cards = tu.get_trapezoids(frame)
     	for card in cards:
-    		hcc.show_image('cards', card)
-	    	hcc_guess = hcc.recognize_card(card)
-	    	print "histogram contour prediction: " 
-	    	print hcc_guess
-	    	guess = wave.recognize_card(card)
-	    	print "wavelet-compression prediction: "
-	    	print guess
+            hcc.show_image('cards', card)
+            hcc_guess = hcc.recognize_card(card)
+            print "histogram contour prediction: " 
+            print hcc_guess
+            guess = wave.recognize_card(card)
+            print "wavelet-compression prediction: "    
+            print hcc_guess
+            print "sift prediction: "
+            print siftcard.cardRecognize(card)
+
+
 
 cap.release()
 cv2.destroyAllWindows()
