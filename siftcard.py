1ebbe31d69787bac445d7f55396a2cd118004ec5
dionbaby <badcoolstone.dion@gmail.com>
2016-08-08 23:33:54 +0800

    Add files via upload

--- siftcard.py
+++ siftcard.py
@@ -3,38 +3,36 @@ import cv2
 #import sift
 import glob
 
-class siftcard(object):
-    def __init__(self):
-        self.cvSift = cv2.SIFT()
-        self.bf = cv2.BFMatcher()
-        self.modelCards = {}
-        for imagePath in glob.glob("*.jpg"):
-			modelCard = cv2.imread(imagePath, 0)
-			modelSiftValues = getSiftFeatureValue(modelCard)
-			cardName = imagePath.replace(".jpg", "")
-			self.modelCards[cardName] = modelSiftValues
+cvSift = cv2.SIFT()
+bf = cv2.BFMatcher()
+modelCards = {}
+for imagePath in glob.glob("*.jpg"):
+	modelCard = cv2.imread(imagePath, 0)
+	modelSiftValues = getSiftFeatureValue(modelCard)
+	cardName = imagePath.replace(".jpg", "")
+	modelCards[cardName] = modelSiftValues
 
-	def getSiftFeatureValue(img):
-		_, des = self.cvSift.detectAndCompute(img, None)
-		#des = sift.getDescriptor(img)
-		#	Converting fails
+def getSiftFeatureValue(img):
+	_, des = cvSift.detectAndCompute(img, None)
+	#des = sift.getDescriptor(img)
+	#	Converting fails
 
-		return des
+	return des
 
-	def cardRecognize(card):	
-		camSiftValues = getSiftFeatureValue(card)
-		maxGood = 6
-		bestMatch = None
+def cardRecognize(card):	
+	camSiftValues = getSiftFeatureValue(card)
+	maxGood = 6
+	bestMatch = None
 
-		for modelCardName,modelDes in self.modelCards.items():	
-			matches = self.bf.knnMatch(camSiftValues, modelDes, k=2)
-			#matches = sift.match(camSiftValues, modelDes)
-			good = 0
-			for m,n in matches:
-				if m.distance < 0.5 * n.distance:
-					good = good + 1
-			if good >= maxGood:
-				maxGood = good
-				bestMatch = modelCardName
+	for modelCardName,modelDes in modelCards.items():	
+		matches = bf.knnMatch(camSiftValues, modelDes, k=2)
+		#matches = sift.match(camSiftValues, modelDes)
+		good = 0
+		for m,n in matches:
+			if m.distance < 0.5 * n.distance:
+				good = good + 1
+		if good >= maxGood:
+			maxGood = good
+			bestMatch = modelCardName
 
-		return bestMatch
+	return bestMatch
